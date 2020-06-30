ed91dc73ba6ca533b93e10196fe96a98f9e8fceb
alan890104 <48343225+alan890104@users.noreply.github.com>
2020-07-01 06:00:32 +0800

    加快版
    不會浪費時間再去重新找網址

--- IOT_WEATHER.py
+++ IOT_WEATHER.py
@@ -11,6 +11,8 @@ import pandas as pd
 import IOT_GPS as GPS
 
 
+addr_dict = {}
+
 def loading():
     import random
     i=0
@@ -29,48 +31,62 @@ def weather(location=None):
 
     if location==None:
         print('這不是在台灣喔!')
-        return
+        return 0
     else:
         print('你所在的位置是 : ',location)
         location = location.split(',')
         location = location[1]+', '+location[0]
 
-        url = 'https://weather.com/zh-TW/weather/today/l/TWXX0021:1:TW'
-       
-        try:
-            options = Options()
-            #options.add_argument("--headless")
-            options.add_experimental_option('excludeSwitches', ['enable-logging'])
-            driver = webdriver.Chrome(executable_path =r'C:\Users\alan8\test543\project\chromedriver.exe',options=options)
-            driver.get(url)
-            time.sleep(1)
-            input_box = WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH,'//*[@id="LocationSearch_input"]')))
-            time.sleep(1)
-            driver.execute_script("arguments[0].click();", input_box)
-            time.sleep(1)
-            input_box = WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH,'//*[@id="LocationSearch_input"]')))
-            time.sleep(1)
-            input_box.send_keys(location)
-            time.sleep(1)
-            input_box.send_keys(Keys.ENTER)
-            
-            wind = driver.find_element_by_xpath('//*[@id="WxuTodayDetails-main-fd88de85-7aa1-455f-832a-eacb037c140a"]/section/div[2]/div[2]/div[2]/span')
-            visible = driver.find_element_by_xpath('//*[@id="WxuTodayDetails-main-fd88de85-7aa1-455f-832a-eacb037c140a"]/section/div[2]/div[7]/div[2]/span')
+        if location not in addr_dict:
+            try:
+                url = 'https://weather.com/zh-TW/weather/today/l/TWXX0021:1:TW'
+                options = Options()
+                #options.add_argument("--headless")
+                options.add_experimental_option('excludeSwitches', ['enable-logging'])
+                driver = webdriver.Chrome(executable_path =r'C:\Users\alan8\test543\project\chromedriver.exe',options=options)
+                driver.get(url)
+                time.sleep(1)
+                input_box = WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH,'//*[@id="LocationSearch_input"]')))
+                time.sleep(1)
+                driver.execute_script("arguments[0].click();", input_box)
+                time.sleep(1)
+                input_box = WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH,'//*[@id="LocationSearch_input"]')))
+                time.sleep(1)
+                input_box.send_keys(location)
+                time.sleep(1)
+                input_box.send_keys(Keys.ENTER)
 
-            print('風速是',wind.text)
-            print('能見度有',visible.text)
+                addr_dict[location] = driver.current_url
+            except:
+                pass
+        else:
+            try:
+                url = addr_dict[location]
+                options = Options()
+                #options.add_argument("--headless")
+                options.add_experimental_option('excludeSwitches', ['enable-logging'])
+                driver = webdriver.Chrome(executable_path =r'C:\Users\alan8\test543\project\chromedriver.exe',options=options)
+                driver.get(url)
+            except:
+                pass
 
-            w,v = wind.text,visible.text
-            driver.quit()
-            return float(w.split()[0]),float(v.split()[0])
+        wind = driver.find_element_by_xpath('//*[@id="WxuTodayDetails-main-fd88de85-7aa1-455f-832a-eacb037c140a"]/section/div[2]/div[2]/div[2]/span')
+        visible = driver.find_element_by_xpath('//*[@id="WxuTodayDetails-main-fd88de85-7aa1-455f-832a-eacb037c140a"]/section/div[2]/div[7]/div[2]/span')
 
-        except:
-            pass
+        print('風速是',wind.text)
+        print('能見度有',visible.text)
+
+        w,v = wind.text,visible.text
+        driver.quit()
+        return float(w.split()[0]),float(v.split()[0])
+
+        
 
 
 if __name__ == '__main__':
     longitude = float(input('經度：'))
     latitude = float(input('緯度：'))
     #loading()
-    print(weather(GPS.your_location(longitude,latitude)))
+    weather(GPS.your_location(longitude,latitude))
+
     
